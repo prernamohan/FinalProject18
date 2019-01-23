1ba03768e0d134d07c55d8f44644de90edf0df37
prernamohan <36172290+prernamohan@users.noreply.github.com>
2019-01-23 00:29:01 -0500

    Comments Added

--- NewCrosswordCode.py
+++ NewCrosswordCode.py
@@ -1,40 +1,48 @@
-
 class Cell:
-
+"""Creates a class named Cell to create a cell where a potential letter could be in the crossword"""
     def __init__(self, x, y, is_blank, value=" "):
-
+#Defines all the arguments
         self.x = x
         self.y = y
         self.is_blank = is_blank
+#The if statement determines that if the cell is blank then the value of that cell is an asterik.
+#  This will be useful later when we print the value of the cell. 
         if self.is_blank:
             self.value = "*"
+#If the cell is not blank then the value of the cell is just the value 
         else:
             self.value = value
 
     def show_cell(self):
         print(self.value, end=" ")
-
 class Grid:
-
+This creates a class called Grid to display a grid that will be the base of the crossword"""
 
     def __init__(self, rows, cols):
+        """This defines the dimensions of the grid in terms of rows and columns"""
+
         self.grid = []
         for y in range(rows):
             row = []
+#This for loop will add cells to the rows and columns to create a grid 
             for x in range(cols):
                 row.append(Cell(x, y, True))
             self.grid.append(row)
 
-  
     def make_word(self, word, direction, x, y):
+        """This function will take a given word and depending on the direction increase the position of each letter by one in the x direction or by one in the y direction."""
+
         for n in range(len(word)):
+#If the direction is horizontal then it will increase x by each n value and then display the value so it shows the word in crossword
             if direction == 'horizontal':
                 self.grid[y][x+n].value = word[n]
+#If the direction is vertical then it will increase y by each n value and then display the value so the word shows vertical in the crossword
             elif direction == 'vertical':
                 self.grid[y+n][x].value = word[n]
 
 
     def show_grid(self):
+        """This function will show the value of the cells in a grid and then print a space and a dotted line to separate the grid from the space where the user enters their guess"""
         for row in self.grid:
             for cell in row:
                 cell.show_cell()
@@ -42,17 +50,21 @@ class Grid:
         print("-"*30)
 
 
-
+#This defines the dimension of the crossword which is 20 rows and 46 columns; It then displays the grid
 grid = Grid(20, 46)
 grid.show_grid()
 
 
-
+#The list word lists the different terms used in the crossword which corresponds to the position listed in list position 
+#This will let the program to check the index of both lists and if they match then the guess is right at that position
+#The list correct_guesses will be list that correct guesses are added to for a final comparison to determine whether the crossword has been solved
 word = ["ARNOLD", "SEMIFORMAL", "NOWAKOSKI", "THINKPOSITIVEANDSTAYATTRACTIVE", "GERSTEIN", "FORTHEBRAND", "OMAY", "THEGREATGATSBY", "MAGNETMADNESS", "MAKERSPACE", "AUTOCADCERTIFICATION", "SPEAKEASY", "RELAYFORLIFE", "ALLISON", "STUDENTGOVERNMENT", "ENGINEERING", "TIMELINE", "SANSERVINO", "APEXAMS", "GUPTA", "JENN", "PYTHON"]
 position = ["H5", "H6", "H7", "H11", "H13", "H15", "H16", "H19", "H20", "V1", "V2", "V3", "V4", "V5", "V6", "V8", "V9", "V10", "V12", "V14", "V17", "V18"] 
 correct_guesses = []
 
 def display_clue():
+    """This function displays the clues with the position in front and then clue following. The letter/number combination 
+    in the beginning will be the position they enter when they guess"""
     print(f"""            H5: The teacher who went to Harvard Law
             H6: The main dance at UCVTS hosted by Student Government
             H7: Literature teacher who told us bizzare stories about his life
@@ -79,11 +91,17 @@ def display_clue():
     
 
 def display_word(self):
+    """This function checks first to see whether the crossword has been completed. If not then it will 
+    check to see what word matches the correct guess of the user and whatever word it matches, it will display
+    that word at the correct location."""
     if correct_guesses == word:
         print("Congratulations! You finished!")
-    elif guess == "ARNOLD": 
+    elif guess == "ARNOLD":
+#The grid.make_word is from a perviously defined function with the arguments given.  
         grid.make_word("ARNOLD", "horizontal", 40, 2)
+#This will display the grid after the word has been added to it. 
         grid.show_grid()
+#This will display the clues once more so the user can continue guessing. This is the same format to check the word for all terms. 
         display_clue()
     elif guess == "SEMIFORMAL":
         grid.make_word("SEMIFORMAL", "horizontal", 3, 3)
@@ -170,19 +188,29 @@ def display_word(self):
         grid.show_grid()
         display_clue()
 
-
+#This is the main code to take user input and determine if it is a correct guess
+#Setting guess_right = 0 helps set up a while loop to let the program run until the crossword is solved
 guess_right = 0
+#This will display the clues so the user can guess
 display_clue()
 while guess_right == 0:
+#Guess_position defines the position they enter and guess defines the user guess
   guess_position = input("Enter position: ").upper()
   guess = input("Enter a guess(no spaces): ").upper()
+#This if statement check to see if the user guess is contained in the list above. If not is prints you're wrong. 
   if guess in word:
+#If the guess is in the list then it assigns guess_index the index number associated with the term the user guessed. 
     guess_index = word.index(guess)
+#This assigns check_position the position at the same, corresponding index number that was returned previously. 
     check_position = position[guess_index]
+#The if statement checks to see if the correct position value at the user guess is the same as the position the user entered to check if the user got the right answer at the right position. 
     if check_position == guess_position:
+#If the if statement is true, it means the user is right and prints you are correct. It then appends the correct guess to the correct_guesses list.
       print("you are correct")
       correct_guesses[guess_index:guess_index] = [guess]
+#This runs the function to check which word the user guessed and whatever word it was, it will display that word in the right place. 
       display_word(guess)
+#This is the else statement from the previous if statement that determined whether the guess was in the word. 
   else:
     print("you were wrong")
     guess_right = 0
